52d3cd39b9cee2aabe6fbfad2cf6c8c2072e0406
Isaac Elias <nicecurelom@gmail.com>
2013-07-15 17:30:57 -0700

    user model : close issue #1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Karmabucket::Application.config.secret_token = '1f8e206bff35f5400ad2f05976772a7251bd2c7b41e207ec0843a889114c6fe05cee20205f48a08e6992179528291c51b9785455a3cd3672ac81eb7c63351eeb'
